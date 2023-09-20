a87c44d55cd7b2bab31f736cc7e055dec3428c6f
nilesh-studium <nilesh@studiumtech.in>
2023-09-20 20:02:03 +0530

    init sharedGPT

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * @format
+ */
+import * as React from 'react';
+import {PaperProvider} from 'react-native-paper';
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import App from './App';
+
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
+AppRegistry.registerComponent(appName, () => Main);
